e0cc2e0cbc68bbd17cc035e782ef108bdaf82c73
Arsam <87975732+arsamhb@users.noreply.github.com>
2025-01-04 18:07:23 +0330

    feat: axios config added

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ This repository leverages the Tailwind for styling, also a global.css for global
 
 ## Setting the Project Up
 
+Add the server address in your ENV file with name this REACT_APP_BASE_URL
+
 If you use remote images in your project go to ./next.config.ts and config the remote pattern for your images
 
 If you want to set a local font (which you have been placed it in ./public/fonts/blabla.woff2) then you should go to src/app/layout and handle a section under comment // FONT CONFIGURATION
@@ -25,3 +27,9 @@ Suggested Options:
     /fonts -> place all of your fonts in this directory
     /img -> place all of your statics images and svg files in this directory
     favicon -> do not forget to add your favicon to the project by replacing it with vercel's icon
+
+# service
+
+This folder contains the configuration for axios, the configuration supports get, post, delete and put methods, also the adapter layer is handled within the configuration.
+
+in /response-default-messages you can add messages that will be helpful for when the server is not providing messages
---
+++ src/service/index.ts
@@ -0,0 +1,75 @@
+import axios, { AxiosError } from "axios";
+import { UNKNOWN_ERROR } from "./response-default-messages/errors";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL ?? "",
+  timeout: 5000,
+});
+
+const post = <T, O>(
+  url: string,
+  local2api: (local: T) => any = (local: T) => local,
+  api2local: (api: any) => O = (api: any) => api
+) => {
+  return async (body: T) => {
+    try {
+      const response = await instance.post(url, local2api(body));
+      return api2local(response.data) as O;
+    } catch (e) {
+      const error = e as AxiosError;
+      return Promise.reject(error.response?.data ?? UNKNOWN_ERROR);
+    }
+  };
+};
+
+const get = <O>(
+  url: string,
+  api2local: (api: any) => O = (api) => api,
+  queryParams: any = {}
+) => {
+  return async () => {
+    try {
+      const response = await instance.get(url, { params: queryParams });
+      return api2local(response.data) as O;
+    } catch (e) {
+      const error = e as AxiosError;
+      return Promise.reject(error.response?.data ?? UNKNOWN_ERROR);
+    }
+  };
+};
+
+const _delete = <T>(url: string) => {
+  return async (queryParams: T) => {
+    try {
+      const response = await instance.delete(url, { params: queryParams });
+      return response.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return Promise.reject(error.response?.data ?? UNKNOWN_ERROR);
+    }
+  };
+};
+
+const put = <T>(
+  url: string,
+  local2api: (local: T) => any = (local: T) => local
+) => {
+  return async (body: T) => {
+    try {
+      const response = await instance.put(url, local2api(body));
+      return response.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      return Promise.reject(error.response?.data ?? UNKNOWN_ERROR);
+    }
+  };
+};
+
+const api = {
+  get,
+  post,
+  delete: _delete,
+  put,
+};
+
+export default api;
