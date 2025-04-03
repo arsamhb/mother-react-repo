42d17a96292b4857e03e5ec37a912df43c660680
Arsam <87975732+arsamhb@users.noreply.github.com>
2025-04-03 18:18:11 +0330

    fix-feat: button component

--- src/shared/UI/Button/button.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface ButtonProps {
-  variant?: 'primary' | 'secondary';
-  children: React.ReactNode;
-  onClick?: () => void;
-}
--- src/shared/UI/Button/index.tsx
+++ src/shared/UI/Button/index.tsx
@@ -1,10 +1,30 @@
+import clsx from 'clsx';
 import React from 'react';
-import { ButtonProps } from './button.interface';
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, variant = 'primary' }) => {
+export interface ButtonProps {
+  variant?: 'primary' | 'secondary';
+  children: React.ReactNode;
+  onClick?: () => void;
+  loading?: boolean;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  variant = 'primary',
+  disabled = false,
+  loading = false,
+}) => {
+  const ButtonClassName = clsx('btn', {
+    'btn-disabled': loading || disabled,
+    'btn-primary': variant === 'primary' && !loading && !disabled,
+    'btn-secondary': variant === 'secondary' && !loading && !disabled,
+  });
+
   return (
-    <button onClick={onClick} className={`custom-button-${variant}`}>
-      {children}
+    <button disabled={disabled || loading} onClick={onClick} className={ButtonClassName}>
+      {loading ? <span className="loading loading-dots loading-sm"></span> : children}
     </button>
   );
 };
